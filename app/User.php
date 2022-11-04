0b1faa77a798996c72211f0af6591dca3cf76c06
Robin Friberg <robin@devpeak.se>
2022-11-04 18:03:46 +0100

    Update docblock from upstream

--- app/User.php
+++ app/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $fillable = [
         'email', 'password',
@@ -31,7 +31,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for arrays.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
@@ -40,7 +40,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be cast to native types.
      *
-     * @var array
+     * @var array<string, string>
      */
     protected $casts = [
         'lastlogin' => 'datetime',
