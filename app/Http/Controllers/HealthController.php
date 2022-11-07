3f62407091cc6d83e3958f9488a11fcace5a5979
Robin Friberg <robin@devpeak.se>
2022-11-07 19:16:29 +0100

    Cleanup HealthController

--- app/Http/Controllers/HealthController.php
+++ app/Http/Controllers/HealthController.php
@@ -2,23 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpFoundation\Response;
 
 class HealthController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
         try {
             $dbname = DB::connection()->getDatabaseName();
-            return response([
+            return response()->json([
                 'status' => 'Database connection OK',
                 'message' => 'Database name: ' . $dbname
             ]);
-        } catch (\Exception $ex) {
-            return response([
+        } catch (Exception $ex) {
+            return response()->json([
                 'status' => 'Database connection lost',
                 'message' => $ex->getMessage()
-            ])->setStatusCode(500);
+            ])->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
