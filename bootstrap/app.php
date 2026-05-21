dc2bd9a4c18cbe8dc6424c2572717d3070b9f7f8
Robin Evermo <robin@devpeak.se>
2026-05-21 22:57:48 +0200

    Upgrade to Laravel 13

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,7 @@ return Application::configure()
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
+            \App\Http\Middleware\PreventRequestForgery::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ]);
 
@@ -34,6 +34,7 @@ return Application::configure()
             'can' => \Illuminate\Auth\Middleware\Authorize::class,
             'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
             'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+            'preventRequestForgery' => \App\Http\Middleware\PreventRequestForgery::class,
             'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
             'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
             'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
