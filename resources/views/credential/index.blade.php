a56441a6c85ea8bca78617db73a06d5ab9d5a56d
Robin Evermo <robin@devpeak.se>
2026-05-29 21:38:49 +0200

    Use new sidebbar layout for 'delete credential'-page

--- resources/views/credential/index.blade.php
+++ resources/views/credential/index.blade.php
@@ -1,6 +1,7 @@
-@extends('layouts.master')
+@php $activeGroupId = $credential->groupid; @endphp
+@extends('layouts.vault')
 @section('content')
-    <div class="container">
+    <div>
         <pwdsafe-alert theme="danger" classes="max-w-2xl mx-auto">
             <p class="mb-4"><strong>Are you sure</strong> you wish to delete credential for "<strong>{{ $credential->site }}</strong>"?</p>
             <form method="post" action="{{ route('credential', $credential->id) }}">
