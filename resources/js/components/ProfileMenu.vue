2f53191e01d10b9008934d3cb1c36f721b2d0895
Robin Evermo <robin@devpeak.se>
2026-06-22 23:46:11 +0200

    Remove name from navbar on mobile (move it to user dropdown)

--- resources/js/components/ProfileMenu.vue
+++ resources/js/components/ProfileMenu.vue
@@ -4,8 +4,8 @@
             <MenuButton
                 class="inline-flex w-full justify-center px-4 py-2 text-sm font-medium text-gray-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 dark:text-gray-300 dark:hover:text-gray-200"
             >
-                <heroicons-user-icon class="mr-1 h-5 w-5"></heroicons-user-icon>
-                {{ username }}
+                <heroicons-user-icon class="h-5 w-5 sm:mr-1"></heroicons-user-icon>
+                <span class="hidden sm:inline">{{ username }}</span>
             </MenuButton>
         </div>
 
@@ -21,6 +21,7 @@
                 class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-200 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none dark:divide-gray-800 dark:bg-gray-600"
             >
                 <div class="py-1">
+                <div class="sm:hidden px-4 py-1.5 text-xs text-gray-400 dark:text-gray-400 truncate">{{ username }}</div>
                     <MenuItem v-slot="{ active }" v-if="isAdmin">
                         <a
                             href="/admin"
