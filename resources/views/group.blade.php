4c720f25080b005e1822dd49e912c10c0bfce0cb
Robin Evermo <robin@devpeak.se>
2026-06-22 23:32:42 +0200

    Make group name length dynamic

--- resources/views/group.blade.php
+++ resources/views/group.blade.php
@@ -21,9 +21,9 @@
     @endif
 
     <div class="flex justify-between mb-5 gap-x-2 items-start">
-        <h3 class="text-2xl flex items-center gap-x-2 text-gray-900 dark:text-gray-100">
+        <h3 class="text-2xl flex items-center gap-x-2 text-gray-900 dark:text-gray-100 min-w-0">
             @if ($group->id !== auth()->user()->primarygroup)
-                <span class="max-w-36 md:max-w-2xl block truncate">{{ $group->name }}</span>
+                <span class="min-w-0 truncate">{{ $group->name }}</span>
             @else
                 Private
             @endif
